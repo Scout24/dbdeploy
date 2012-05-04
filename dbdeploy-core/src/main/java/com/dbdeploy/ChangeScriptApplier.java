3956da8948528c83b689cba4c35addc7613beb68
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2012-05-04 18:11:45 +0200

    minimal formatting

--- dbdeploy-core/src/main/java/com/dbdeploy/ChangeScriptApplier.java
+++ dbdeploy-core/src/main/java/com/dbdeploy/ChangeScriptApplier.java
@@ -1,8 +1,8 @@
 package com.dbdeploy;
 
 import com.dbdeploy.scripts.ChangeScript;
-
 import java.util.List;
+
 
 public interface ChangeScriptApplier {
 	void apply(List<ChangeScript> changeScript);
